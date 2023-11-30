7f27e0a716b3246a9b759c1c2f82585340ce0da1
Dzaki Arrozaq <dzakiarrozaq62@students.unnes.ac.id>
2023-11-30 23:53:24 +0700

    masih eror dibagian custom view

---
+++ app/src/main/java/com/bangkit/upcycle/customview/CVName.kt
@@ -0,0 +1,119 @@
+package com.bangkit.upcycle.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.bangkit.upcycle.R
+
+class CVName : AppCompatEditText, View.OnTouchListener {
+    private lateinit var clearButtonImage: Drawable
+    private lateinit var iconFormInput: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    private fun init() {
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.cross) as Drawable
+        iconFormInput = ContextCompat.getDrawable(context, R.drawable.orang) as Drawable
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) showClearButton() else hideClearButton()
+            }
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ){
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables(startOfTheText = iconFormInput)
+    }
+
+    private fun showClearButton() {
+        setButtonDrawables(startOfTheText = iconFormInput, endOfTheText = clearButtonImage)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.silang) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.silang) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        return true
+                    }
+                    else -> return false
+                }
+            } else return false
+        }
+        return false
+    }
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        context.apply {
+            setTextColor(ContextCompat.getColor(this, R.color.black))
+            setHintTextColor(ContextCompat.getColor(this, R.color.black))
+            background = ContextCompat.getDrawable(this, R.drawable.form_input)
+        }
+        isSingleLine = true
+
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+    }
+
+
+}
---
+++ app/src/main/java/com/bangkit/upcycle/customview/CVPassword.kt
@@ -0,0 +1,87 @@
+package com.bangkit.upcycle.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.bangkit.upcycle.R
+
+class CVPassword : AppCompatEditText {
+    private lateinit var iconFormInput: Drawable
+    private var characterLength = 0
+
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+
+
+
+
+    private fun init() {
+        iconFormInput = ContextCompat.getDrawable(context, R.drawable.lock) as Drawable
+        showIconFormInput()
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                characterLength = s.length
+                if (!s.isNullOrEmpty() && characterLength < 8) error = context.getString(R.string.label_validation_password)
+            }
+            override fun afterTextChanged(edt: Editable?) {
+
+            }
+        })
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ){
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    private fun showIconFormInput() {
+        setButtonDrawables(startOfTheText = iconFormInput)
+    }
+
+
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        context.apply {
+            setTextColor(ContextCompat.getColor(this, R.color.black))
+            setHintTextColor(ContextCompat.getColor(this, R.color.black))
+            background = ContextCompat.getDrawable(this, R.drawable.form_input)
+        }
+        maxLines = 1
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+}
