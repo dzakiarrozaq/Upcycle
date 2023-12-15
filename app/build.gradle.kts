cd5178acd1af76456f9b180b15f5b965c6da2975
Thoga12 <thogatflash@gmail.com>
2023-12-15 23:22:51 +0800

    Register Error Pada Endpoint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -26,9 +28,6 @@ android {
             )
         }
     }
-    buildFeatures{
-        viewBinding = true
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -38,6 +37,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -52,4 +52,32 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.8.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+//    implementation ("androidx.room:room-ktx:2.6.0")
+//    kapt ("androidx.room:room-compiler:2.6.0")
+
+    testImplementation("androidx.arch.core:core-testing:2.2.0") // InstantTaskExecutorRule
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3") //TestDispatcher
+    testImplementation("org.mockito:mockito-core:4.4.0")
+    testImplementation("org.mockito:mockito-inline:4.4.0")
+
+    implementation("androidx.room:room-paging:2.6.1")
+    implementation ("androidx.room:room-runtime:2.6.1")
 }
--- app/src/main/java/com/bangkit/upcycle/register/SignUpActivity.kt
+++ app/src/main/java/com/bangkit/upcycle/register/SignUpActivity.kt
@@ -1,12 +1,94 @@
-package com.bangkit.upcycle.ui
+package com.bangkit.upcycle.register
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.bangkit.upcycle.R
+import com.bangkit.upcycle.ViewModelFactory
+import com.bangkit.upcycle.databinding.ActivitySignUpBinding
+import com.bangkit.upcycle.login.LoginActivity
+import com.bangkit.upcycle.response.ErrorResponse
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class SignUpActivity : AppCompatActivity() {
+
+
+    private val viewModel by viewModels<RegisterViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
+    private var _binding: ActivitySignUpBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        _binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signupButton.setOnClickListener{
+            register()
+        }
+    }
+
+    private fun register() {
+        Log.d("Tes","Proses Register Dijalankan dengan baik ")
+        if(binding.tvRegisterName.text!!.isEmpty()){
+            binding.tvRegisterName.error = "Kolom Nama Tidak Boleh Kosong"
+            binding.tvRegisterName.requestFocus()
+            return
+        } else if(binding.tvRegisterEmail.text!!.isEmpty()) {
+            binding.tvRegisterEmail.error = "Kolom Email Tidak Boleh Kosong"
+            binding.tvRegisterEmail.requestFocus()
+            return
+        }else if(binding.tvRegisterPassword.text!!.isEmpty()) {
+            binding.tvRegisterPassword.error = "Kolom Password Tidak Boleh Kosong"
+            binding.tvRegisterPassword.requestFocus()
+            return
+        }
+
+        val name = binding.tvRegisterName.text.toString()
+        val email = binding.tvRegisterEmail.text.toString()
+        val password = binding.tvRegisterPassword.text.toString()
+
+        lifecycleScope.launch {
+            try {
+                val response = viewModel.register(name, email, password)
+                showLoading(false)
+                Log.d("RegisterActivity", "Registrasi berhasil: $response")
+                val message = "Registrasi berhasil: $response"
+                showToast(message)
+                AlertDialog.Builder(this@SignUpActivity).apply {
+                    setTitle("Yei!")
+                    setMessage(getString(R.string.register_succes))
+                    setPositiveButton(getString(R.string.continuee)) { _, _ ->
+                        val intent = Intent(context, LoginActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                        finish()
+                    }
+                    create()
+                    show()
+                }
+            } catch (e: HttpException) {
+                val jsonInString = e.response()?.errorBody()?.string()
+                val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
+                val errorMessage = errorBody.message
+                showToast(errorMessage.toString())
+                showLoading(false)
+            }
+        }
+    }
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun showLoading(isLoading: Boolean) {
+        binding.progressBarRegister.visibility = if (isLoading) android.view.View.VISIBLE else android.view.View.GONE
+        binding.signupButton.isEnabled = !isLoading
     }
 }
